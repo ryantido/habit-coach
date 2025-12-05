8ece4e875d5e4d0a31b007eebc95d7fec32d01f7
ryantido34 <ryantido34@gmail.com>
2025-12-05 14:47:49 +0100

    feat(layout): add MotionWrapper utility component

---
+++ src/components/layouts/MotionWrapper.tsx
@@ -0,0 +1,19 @@
+import { motion } from "motion/react";
+
+export const MotionWrapper = ({ children, ariaLabel }: { children: React.ReactNode, ariaLabel: string }) => {
+  return (
+    <motion.section
+      initial={{ opacity: 0, y: 15, x: -15 }}
+      animate={{ opacity: 1, y: 0, x: 0 }}
+      transition={{
+        type: "spring",
+        stiffness: 300,
+        damping: 15,
+      }}
+      aria-label={ariaLabel}
+      className="w-full"
+    >
+      {children}
+    </motion.section>
+  );
+};
