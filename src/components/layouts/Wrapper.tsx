23f475ebbc220f09b01ab16799cefce8c142b41d
ryantido34 <ryantido34@gmail.com>
2025-12-05 14:43:49 +0100

    design(spacing): adjust body spacing and add no-scrollbar utility\n\n- add .spacing to body\n- add .no-scrollbar util and apply overflow to Wrapper

--- src/components/layouts/Wrapper.tsx
+++ src/components/layouts/Wrapper.tsx
@@ -2,6 +2,6 @@ import type { ReactNode } from "react";
 
 export const Wrapper = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="container mx-auto size-full px-6 py-16">{children}</div>
+    <div className="container mx-auto size-full px-6 py-16 overflow-y-auto no-scrollbar">{children}</div>
   );
 }
